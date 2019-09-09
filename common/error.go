e6eed6a012431c011f00fd623bc801b841573f1b
zhangdaoling <zhangdaoling@huobi.com>
2019-09-09 09:41:41 +0800

    add err

---
+++ common/error.go
@@ -0,0 +1,10 @@
+package common
+
+import "errors"
+
+var ErrIrregularData = errors.New("irregular data")
+var ErrUnexpectedEOF = errors.New("unexpected EOF")
+var ErrTooLarge = errors.New("bytes.Buffer: too large")
+var ErrExpectedEOF = errors.New("Expected EOF")
+
+var ErrQueueType = errors.New("irregular queue type")
